0fc1c44a9ee0ebcdbc0dc4ab3007dd4401faa4d0
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-01-25 23:36:59 +0500

    add fields to user table

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,6 +1,43 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      requiered: [true, "Please Enter Your Name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please Enter Your Email"],
+      unique: true,
+      trim: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please Enter Valid Email"],
+    },
+    password: {
+      type: String,
+      required: [True, "Please Enter a Password"],
+      minLength: [6, "Password must contains 6 characters"],
+      maxLength: [23, "Password not exceed 23 characters"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Add your photo"],
+      default: "",
+    },
+    phone: {
+      type: String,
+      default: "+92",
+    },
+    bio: {
+      type: String,
+      maxLength: [250, "Biography not exceed 250 characters"],
+      default: "Biography",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
