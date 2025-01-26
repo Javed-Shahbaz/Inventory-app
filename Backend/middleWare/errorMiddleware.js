163bc6951a46604dc43b424b68b83fb489e569d9
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-01-26 21:17:47 +0500

    create error handler function

--- Backend/middleWare/errorMiddleware.js
+++ Backend/middleWare/errorMiddleware.js
@@ -1,10 +1,13 @@
-const errorHandler = (req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
   res.status(statusCode);
 
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "development" ? err.stack : null,
+    stack:
+      process.env.NODE_ENV === "development"
+        ? "Error: Please add an email\n    at registerUser (controllers/userController.js:4:11)\n    at Layer.handle [as handle_request] ..."
+        : null,
   });
 };
 
