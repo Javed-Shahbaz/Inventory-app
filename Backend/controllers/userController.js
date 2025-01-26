f16a72c4d0819f14974dca7999a0437626e86288
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-01-26 22:03:38 +0500

    update user controller

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,7 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
-
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
