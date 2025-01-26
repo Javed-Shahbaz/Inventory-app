c22dca2c5ca25628045bf6c6e49b26d05df12c8b
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-01-26 22:03:17 +0500

    update  user Controller

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,9 +1,32 @@
-const registerUser = (req, res) => {
-  if (!req.body.email) {
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //validation for empty field, password, duplicate email
+
+  if (!name || !email || !password) {
     res.status(400);
-    throw new Error("Please add an email");
+    throw new Error("Please fill blanck field/s");
   }
-  res.send("Register User");
-};
+
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("Password must have atleast 6 characters");
+  }
+  const userExists = await User.findone({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+    // creat new user
+    const user = await User.create({
+      name, //(name: name)
+      email,
+      password,
+    });
+  }
+});
 
 module.exports = { registerUser };
