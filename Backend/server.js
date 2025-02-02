395250f9d9ee654f8bd3fa8974c45d16ab331fb8
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-02-02 22:17:18 +0500

    update middleware

--- Backend/server.js
+++ Backend/server.js
@@ -5,12 +5,13 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoute = require("./routes/userRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 //Middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
 app.use(bodyParser.json());
 
 //Routes Middleware
