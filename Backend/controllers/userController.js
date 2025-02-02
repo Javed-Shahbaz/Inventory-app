9d8075c8d017040013bd5a78099fdd365e8ae31c
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-02-02 22:08:18 +0500

    generate token for user

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,6 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
+// Generating Token for Authentication
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_KEY, { expiresIn: "1d" });
+};
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -29,6 +34,9 @@ const registerUser = asyncHandler(async (req, res) => {
     password,
   });
 
+  //Generate Token
+  const token = generateToken(user._id);
+
   if (user) {
     const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
@@ -38,6 +46,7 @@ const registerUser = asyncHandler(async (req, res) => {
       photo,
       phone,
       bio,
+      token,
     });
   } else {
     res.status(400);
