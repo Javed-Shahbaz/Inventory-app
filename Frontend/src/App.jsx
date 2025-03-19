3541d6503f4290ec0e90b01a4d20f791fea57ae0
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-03-19 06:27:18 +0500

    create  pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import SignupPage from "../pages/SignupPage";
-import LoginPage from "../pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-900 via-gray-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
       <Routes>
-        <Route path="/" element={"Home"} />
+        <Route path="/" element={<div>Home</div>} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
