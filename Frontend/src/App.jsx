8f6e932cdc5990d79328d99d238aed43ba559f1b
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-03-30 06:52:02 +0500

    update changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-900 via-gray-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={"Home"} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
--- Frontend/src/pages/SignupPage.jsx
+++ Frontend/src/pages/SignupPage.jsx
@@ -1,28 +1,41 @@
 import { motion } from "framer-motion";
+import { Input } from "components/Input";
+import { User } from "lucide-react";
+import { useState } from "react";
+
 const SignupPage = () => {
+  const [name, setName] = useState("");
+
   const handleSignup = (e) => {
     e.preventDefault();
+    console.log("Sign Up Attempted:", name);
   };
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="max-w-md w-full bg-yellow-100 bg-opacity-5 backdrop-filter  rounded-2xl shadow-xl overflow-hidden"
+      className="max-w-md w-full bg-yellow-100 bg-opacity-5 backdrop-filter rounded-2xl shadow-xl overflow-hidden p-8"
     >
-      <div className="p-8">
-        <h2
-          className="text-3xl font-bold mb-6"
-          bg-gradient-to-r
-          from-yellow-400
-          to-emerald-500
-          text-transparent
-          bg-clip-text
+      <h2 className="text-3xl font-bold mb-6 bg-gradient-to-r from-yellow-400 to-emerald-500 text-transparent bg-clip-text">
+        Create Account
+      </h2>
+      <form onSubmit={handleSignup}>
+        <Input
+          icon={<User />}
+          type="text"
+          placeholder="Full Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="w-full mt-4 py-2 bg-emerald-500 text-white rounded-lg hover:bg-emerald-600 transition"
         >
-          Create Account
-        </h2>
-        <form onSubmit={handleSignup}></form>
-      </div>
+          Sign Up
+        </button>
+      </form>
     </motion.div>
   );
 };
